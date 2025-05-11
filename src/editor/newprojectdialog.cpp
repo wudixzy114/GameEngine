ac9e1e0ae4eed90d737b46e53f996016031e1902
wudixzy <1712698062@qq.com>
2025-05-11 19:28:42 +0800

    Update NewProjectDialog UI and logic

--- src/editor/newprojectdialog.cpp
+++ src/editor/newprojectdialog.cpp
@@ -5,37 +5,45 @@
 #include <spdlog/spdlog.h>
 
 
+// 构造函数，初始化对话框
 NewProjectDialog::NewProjectDialog(QWidget *parent) : QDialog(parent), ui(new Ui::NewProjectDialog) {
     ui->setupUi(this);
     setWindowTitle("Create a New Project");
 
+    // 连接按钮的点击事件到相应的槽函数
     connect(ui->Browser_PB, &QPushButton::clicked, this, &NewProjectDialog::selectProjectPath);
     connect(ui->Cancel_PB, &QPushButton::clicked, this, &NewProjectDialog::reject);
     connect(ui->Create_PB, &QPushButton::clicked, this, &NewProjectDialog::accept);
     connect(ui->ProjectPath_LE, &QLineEdit::textChanged, this, &NewProjectDialog::validateInputs);
     connect(ui->ProjectName_LE, &QLineEdit::textChanged, this, &NewProjectDialog::validateInputs);
 
+    // 验证输入
     validateInputs();
 }
 
+// 析构函数，删除ui指针
 NewProjectDialog::~NewProjectDialog() {
     delete ui;
 }
 
+// 获取项目名称
 QString NewProjectDialog::getProjectName() const {
     return ui->ProjectName_LE->text().trimmed();
 }
 
+// 获取项目路径
 QString NewProjectDialog::getProjectPath() const {
     return ui->ProjectPath_LE->text().trimmed();
 }
 
+// 选择项目路径
 void NewProjectDialog::selectProjectPath() {
     QString startPath = ui->ProjectPath_LE->text().trimmed();
     if (startPath.isEmpty()) {
-        startPath = QDir::homePath();
+        startPath = QDir::currentPath();
     }
 
+    // 弹出文件选择对话框，选择项目路径
     QString dir = QFileDialog::getExistingDirectory(this, tr("choose path"), startPath);
     if (!dir.isEmpty()) {
         ui->ProjectPath_LE->setText(dir);
@@ -44,24 +52,30 @@ void NewProjectDialog::selectProjectPath() {
         spdlog::info("Project path selection cancelled");
     }
 
+    // 验证输入
     validateInputs();
 }
 
+// 验证输入
 void NewProjectDialog::validateInputs() {
     bool isNameValid = this->isNameValid();
     if (!isNameValid) {
         ui->Create_PB->setEnabled(false);
+        ui->ProjectState_LB->setStyleSheet("QLabel { color: red; }");
         return;
     }
     bool isPathValid = this->isPathValid();
     if (!isPathValid) {
         ui->Create_PB->setEnabled(false);
+        ui->ProjectState_LB->setStyleSheet("QLabel { color: red; }");
         return;
     }
     ui->ProjectState_LB->setText("OK");
+    ui->ProjectState_LB->setStyleSheet("QLabel { color: green; }");
     ui->Create_PB->setEnabled(true);
 }
 
+// 验证路径是否有效
 bool NewProjectDialog::isPathValid() const {
     QString path = getProjectPath();
     if (path.isEmpty()) {
@@ -86,6 +100,7 @@ bool NewProjectDialog::isPathValid() const {
     return true;
 }
 
+// 验证名称是否有效
 bool NewProjectDialog::isNameValid() const {
     if (getProjectName().isEmpty()) {
         ui->ProjectState_LB->setText("Name is Empty");
